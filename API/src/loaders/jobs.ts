32bc0dab259e3eefe6d0c98b6811d117db8188ef
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2024-09-27 23:40:14 +0300

    fixed server setup to run cd to src then run nodemon app.ts

--- API/src/loaders/jobs.ts
+++ API/src/loaders/jobs.ts
@@ -1,13 +1,14 @@
 import config from '@/config';
 import EmailSequenceJob from '@/jobs/emailSequence';
-import Agenda, { JobPriority } from 'agenda';
+import Agenda, { JobPriority, Job } from 'agenda';
 
 export default ({ agenda }: { agenda: Agenda }) => {
   agenda.define(
-    'send-email',
-    { priority: JobPriority.high, concurrency: config.agenda.concurrency },
-    // @TODO Could this be a static method? Would it be better?
-    new EmailSequenceJob().handler,
+    'send-email', 
+    //{ priority: JobPriority.high, concurrency: config.agenda.concurrency },
+    async (job: Job, done: (err?: any) => void) => {
+        await new EmailSequenceJob().handler(job, done);
+    }
   );
 
   agenda.start();
