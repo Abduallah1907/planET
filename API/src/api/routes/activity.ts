26acc13cc715415bd350fd27c4d466f972e52a5b
Aly Basyouni <alybasyouni2003@gmail.com>
2024-09-28 17:01:34 +0300

    Update activity.ts

--- API/src/api/routes/activity.ts
+++ API/src/api/routes/activity.ts
@@ -1,3 +1,7 @@
 import { Router, Request, Response } from "express";
 const Activity = require("../../models/Activity");
 const ActivityID = require("../../interfaces/IActivity");
+
+const createActivity=async (req:Request,res:Response)=>{
+    
+}
