31966d3c14608bf31870ca9f15c08a15ed8e2dc7
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2024-09-29 11:34:44 +0300

    added swagger for better api access

--- API/src/api/routes/user.ts
+++ API/src/api/routes/user.ts
@@ -4,7 +4,20 @@ const route = Router();
 
 export default (app: Router) => {
   app.use('/users', route);
-  
-
+   /**
+   * @swagger
+   * tags:
+   *   - name: Users
+   *     description: User management and retrieval
+   * /api/users/test:
+   *   get:
+   *     tags:
+   *       - Users
+   *     summary: Retrieve a list of users
+   *     description: Retrieve a list of users from the database
+   *     responses:
+   *       200:
+   *         description: A list of users.
+   */
   route.get('/test', userController.test);
 };
