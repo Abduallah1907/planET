745314c2d01dab034f01d7eff0b94ea54edd0056
omarmosallam <omar.m.mohamed.om@gmail.com>
2024-09-29 18:59:27 +0300

    adding flags icons and translation (شعبولي) but shaghal zy el fol changing buttons into dropdown buttons

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import TopBar from './components/TopBar';
-import { BrowserRouter as Router ,Route , Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
 const App: React.FC = () => {
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TopBar />} />
       </Routes>
     </Router>
-
   );
 }
 
