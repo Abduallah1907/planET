18f5c02fc4525d3e3434ad52d21a41dc147ef5eb
Abduallah <abduallahaymanezz@gmail.com>
2024-09-30 21:32:15 +0200

    create admin and governer page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import TopBar from './components/TopBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-const App: React.FC = () => {
+import React from "react";
+import TopBar from "./components/TopBar";
+import CreateAdmin from "./views/CreateAdmin";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import CreateGoverner from "./views/CreateGoverner";
+import BookingLayout from "./views/CreateActivities";
 
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TopBar />} />
+        <Route path="/admin" element={<CreateAdmin />} />
+        <Route path="/governer" element={<CreateGoverner />} />
+        <Route path="/test" element={<BookingLayout />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ Frontend/src/views/CreateGoverner.tsx
@@ -0,0 +1,129 @@
+import React from "react";
+//import Form from "react-bootstrap/Form";
+//import Button from "react-bootstrap/Button";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
+//import { useNavigate } from "react-router-dom";
+
+function CreateGoverner() {
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted");
+  };
+  return (
+    <Form onSubmit={handleSubmit}>
+      <h2 className="mb-4">Create tourism governer account</h2>
+
+      <Row>
+        {/* Email Address on the Left */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>First name</Form.Label>
+            <Form.Control type="name" placeholder="Enter First name" />
+          </Form.Group>
+        </Col>
+
+        {/* Password on the Right */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Last name</Form.Label>
+            <Form.Control type="name " placeholder="Enter Last name" />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        {/* Email Address on the Left */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+        </Col>
+
+        {/* Password on the Right */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Username </Form.Label>
+            <Form.Control type="name" placeholder="Enter username" />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        {/*  Password */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label> Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter Password" />
+          </Form.Group>
+        </Col>
+
+        {/* Confirm password */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label> Confirm password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Please confirm your password"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        {/*  Password */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label> Nationality</Form.Label>
+            <Form.Control
+              type="nationality"
+              placeholder="Enter your Nationality"
+            />
+          </Form.Group>
+        </Col>
+
+        {/* Confirm password */}
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label> Phone number</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Please enter your phone number"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col md={6}>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox1">
+            <Form.Check type="checkbox" label="Remember me" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox2">
+            <Form.Check
+              type="checkbox"
+              label="I agree to all Terms and Privacy Policy"
+            />
+          </Form.Group>
+        </Col>
+      </Row>
+
+      <div className="d-flex justify-content-center mt-4">
+        <Button variant="main-inverse" type="submit" className="btn-lg">
+          Create account
+        </Button>
+      </div>
+
+      <div className="text-center mt-3">
+        <p>
+          Already have an account? <a href="/login">Login here</a>.
+        </p>
+      </div>
+    </Form>
+  );
+}
+
+export default CreateGoverner;
