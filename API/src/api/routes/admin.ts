8ae0d8002b070db5297ff52896f2493cc83c51d9
yousef-fadel <yousef_758@outlook.com>
2024-10-01 13:27:46 +0300

    Add skeleton for admin API documentation
    This is not the final documentation, it is very wrong, estano 3alya

--- API/src/api/routes/admin.ts
+++ API/src/api/routes/admin.ts
@@ -5,8 +5,347 @@ const router = Router();
 // all routes have /api/admin before each route
 
 export default (app: Router) => {
-  app.use("/admin", router);
   const adminController: AdminController = Container.get(AdminController);
+  app.use("/admin", router);
+  /*openapi: 3.0.0
+   *info:
+   *  title: Admin API
+   *  version: 1.0.0
+   *  description: API for managing users and categories in the admin panel.
+   *servers:
+   *  - url: http://localhost:3000/api/admin
+   *tags:
+   *  - name: Users
+   *    description: Operations related to user management.
+   *  - name: Categories
+   *    description: Operations related to category management.
+   *
+   *paths:
+   *  # User Operations
+   *  /getUsers:
+   *    get:
+   *      tags:
+   *        - Users
+   *      summary: Retrieve a list of 10 users according to page number.
+   *      parameters:
+   *        - name: page
+   *          in: query
+   *          description: Page number to retrieve (default is 1).
+   *          required: false
+   *          schema:
+   *            type: integer
+   *            default: 1
+   *      responses:
+   *        '200':
+   *          description: A list of users.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: array
+   *                items:
+   *                  type: object
+   *                  properties:
+   *                    email:
+   *                      type: string
+   *                      example: Metfasa7@gmail.com
+   *                    name:
+   *                      type: string
+   *                      example: Medhat
+   *                    username:
+   *                      type: string
+   *                      example: MedhatMetfasa7
+   *                    role:
+   *                      type: string
+   *                      example: TOURIST
+   *                    phone_number:
+   *                      type: string
+   *                      example: 011111
+   *                    status:
+   *                      type: string
+   *                      example: APPROVED
+   *                      enum:
+   *                        - APPROVED
+   *                        - WAITING_FOR_APPROVAL
+   *                        - REJECTED
+   *                    createdAt:
+   *                      type: string
+   *                      example: 2024-10-01T09:36:47.869Z
+   *
+   *                    updatedAt:
+   *                      type: string
+   *                      example: 2024-10-01T09:36:47.869Z
+   *        '500':
+   *          description: Internal Server Error.
+   *
+   *  /searchUser:
+   *    get:
+   *      tags:
+   *        - Users
+   *      summary: Search for a user by username.
+   *      parameters:
+   *        - name: username
+   *          in: query
+   *          description: Username to search for.
+   *          required: true
+   *          schema:
+   *            type: string
+   *      responses:
+   *        '200':
+   *          description: User found or empty data.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  user:
+   *                    type: object
+   *                    properties:
+   *                      id:
+   *                        type: string
+   *                      username:
+   *                        type: string
+   *                      email:
+   *                        type: string
+   *        '404':
+   *          description: User not found.
+   *
+   *  /deleteUser:
+   *    delete:
+   *      tags:
+   *        - Users
+   *      summary: Delete a user by ID.
+   *      parameters:
+   *        - name: id
+   *          in: query
+   *          description: ID of the user to delete.
+   *          required: true
+   *          schema:
+   *            type: string
+   *      responses:
+   *        '200':
+   *          description: User deleted successfully.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  deletedUser:
+   *                    type: object
+   *                    properties:
+   *                      id:
+   *                        type: string
+   *                      username:
+   *                        type: string
+   *                      email:
+   *                        type: string
+   *        '404':
+   *          description: User not found.
+   *        '500':
+   *          description: Internal Server Error.
+   *
+   *  /createGovernor:
+   *    post:
+   *      tags:
+   *        - Users
+   *      summary: Create a new governor account.
+   *      requestBody:
+   *        required: true
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                email:
+   *                  type: string
+   *                name:
+   *                  type: string
+   *                phone:
+   *                  type: string
+   *                username:
+   *                  type: string
+   *                password:
+   *                  type: string
+   *      responses:
+   *        '201':
+   *          description: Governor created successfully.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  governor:
+   *                    type: object
+   *                    properties:
+   *                      id:
+   *                        type: string
+   *                      username:
+   *                        type: string
+   *                      email:
+   *                        type: string
+   *        '400':
+   *          description: Invalid input.
+   *
+   *  /createAdmin:
+   *    post:
+   *      tags:
+   *        - Users
+   *      summary: Create a new admin account.
+   *      requestBody:
+   *        required: true
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                email:
+   *                  type: string
+   *                name:
+   *                  type: string
+   *                phone:
+   *                  type: string
+   *                username:
+   *                  type: string
+   *                password:
+   *                  type: string
+   *      responses:
+   *        '201':
+   *          description: Admin created successfully.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  admin:
+   *                    type: object
+   *                    properties:
+   *                      id:
+   *                        type: string
+   *                      username:
+   *                        type: string
+   *                      email:
+   *                        type: string
+   *        '400':
+   *          description: Invalid input.
+   *
+   *  # Category Operations
+   *  /createCategory:
+   *    post:
+   *      tags:
+   *        - Categories
+   *      summary: Create a new category.
+   *      requestBody:
+   *        required: true
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                name:
+   *                  type: string
+   *      responses:
+   *        '201':
+   *          description: Category created successfully.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  category:
+   *                    type: object
+   *                    properties:
+   *                      id:
+   *                        type: string
+   *                      name:
+   *                        type: string
+   *        '400':
+   *          description: Invalid input.
+   *
+   *  /getCategories:
+   *    get:
+   *      tags:
+   *        - Categories
+   *      summary: Retrieve a paginated list of categories.
+   *      parameters:
+   *        - name: page
+   *          in: query
+   *          description: Page number to retrieve (default is 1).
+   *          required: false
+   *          schema:
+   *            type: integer
+   *            default: 1
+   *      responses:
+   *        '200':
+   *          description: A list of categories.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: array
+   *                items:
+   *                  type: object
+   *                  properties:
+   *                    id:
+   *                      type: string
+   *                    name:
+   *                      type: string
+   *        '500':
+   *          description: Internal Server Error.
+   *
+   *  /updateCategory:
+   *    put:
+   *      tags:
+   *        - Categories
+   *      summary: Update an existing category name.
+   *      requestBody:
+   *        required: true
+   *        content:
+   *          application/json:
+   *            schema:
+   *              type: object
+   *              properties:
+   *                oldName:
+   *                  type: string
+   *                newName:
+   *                  type: string
+   *      responses:
+   *        '200':
+   *          description: Category updated successfully.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message:
+   *                    type: string
+   *        '404':
+   *          description: Category not found.
+   *        '400':
+   *          description: Invalid input.
+   *
+   *  /deleteCategory:
+   *    delete:
+   *      tags:
+   *        - Categories
+   *      summary: Delete a category by name.
+   *      parameters:
+   *        - name: name
+   *          in: query
+   *          description: Name of the category to delete.
+   *          required: true
+   *          schema:
+   *            type: string
+   *      responses:
+   *        '200':
+   *          description: Category deleted successfully.
+   *          content:
+   *            application/json:
+   *              schema:
+   *                type: object
+   *                properties:
+   *                  message:
+   *                    type: string
+   *        '404':
+   *          description: Category not found.
+   */
   // This returns all users given a page number
   // Each page has 10 users
   router.get("/getUsers", adminController.getUsers);
