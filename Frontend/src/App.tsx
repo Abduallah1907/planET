08f67a2621412e6601f06e8114ed9679f0464340
Omar Mohamed Abdelghafar <omarmohamed9594@gmail.com>
2024-10-01 14:47:30 +0300

    FormGroup && Edit Profile page and nationality list for all pages

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import TopBar from './components/TopBar';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-const App: React.FC = () => {
+import React from "react";
+import TopBar from "./components/TopBar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ProfileForm from "./components/ProfileForm/ProfileForm";
+import CustomFormGroup from "./components/FormGroup";
+import { Placeholder } from "react-bootstrap";
 
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<TopBar />} />
+        <Route path="/editprofile" Component={ProfileForm} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
