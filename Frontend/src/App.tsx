44dfc9bbdbedec6c6d5e68febb0f19e9b0d48519
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2024-10-02 17:27:51 +0300

    Relative pathing and react redux store

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,20 +1,16 @@
 import React from "react";
 import TopBar from "./components/TopBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import ProfileForm from "./components/ProfileForm/ProfileForm";
-import CustomFormGroup from "./components/FormGroup";
-import { Placeholder } from "react-bootstrap";
 
 const App: React.FC = () => {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<TopBar />} />
         <Route path="/editprofile" Component={ProfileForm} />
       </Routes>
-    </Router>
   );
 };
 
--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -4,15 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import store from '@/store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
-  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
