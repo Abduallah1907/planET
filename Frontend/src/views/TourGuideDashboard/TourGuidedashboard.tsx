a2f630b67ff26b1b2a55dfe42e6f537fba2e2865
Abdelrahman Tamer <abdelrahmantamer2005@yahoo.com>
2024-10-05 14:32:24 +0300

    Refactor import paths for TopBar component

--- Frontend/src/views/TourGuideDashboard/TourGuidedashboard.tsx
+++ Frontend/src/views/TourGuideDashboard/TourGuidedashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import TopBar from '../../components/TopBar';
+import TopBar from '../../components/TopBar/TopBar';
 import StatisticsCards from './StatisticsCards';
 import CountryCampaignTable from './CountryCampaignTable';
 import './TourGuidedashboard.css';
