053cd74d082814272873127c5528d17fed82ebe4
Aly Basyouni <alybasyouni2003@gmail.com>
2024-11-16 18:41:19 +0200

    More implememtation

--- API/src/api/controllers/notificationController.ts
+++ API/src/api/controllers/notificationController.ts
@@ -5,19 +5,6 @@ import { Types } from "mongoose";
 
 @Service()
 export default class NotificationController {
-  //Create notification
-  public async createNotification(req: any, res: any) {
-    const notificationService: NotificationService =
-      Container.get(NotificationService);
-    const notificationData = req.body;
-    const notification = await notificationService.createNotificationService(
-      notificationData.notified_id,
-      notificationData.message,
-      notificationData.user_type
-    );
-    res.status(notification.status).json(notification);
-  }
-
   //Get all notifications using id
   public async getNotificationsById(req: any, res: any) {
     const { id } = req.params;
